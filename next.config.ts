3b6fb4ea951e28fccc39afa4f1dae83316690192
adrian0silva <adrianosilvaparola@gmail.com>
2026-01-10 23:45:01 -0300

    feat: Adicionar configuração de cache e atualizar dependências do projeto

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  cacheComponents: true,
   typedRoutes: true,
   eslint: {
     ignoreDuringBuilds: true,
@@ -9,10 +10,10 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "sdfsf5s5sd.cloudfront.net"
-      }
-    ]
-  }
+        hostname: "sdfsf5s5sd.cloudfront.net",
+      },
+    ],
+  },
   /* config options here */
 };
 
